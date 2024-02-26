658eb27903c82dbe5dbae7ccbb7aa3be64edf79d
Renato <renatodepsf@gmail.com>
2024-02-26 20:45:58 -0300

    salvando as alteracoes

--- med/src/main/java/com/voll/med/controller/MedicoController.java
+++ med/src/main/java/com/voll/med/controller/MedicoController.java
@@ -1,16 +1,20 @@
 package com.voll.med.controller;
 
 import com.voll.med.domain.Medicos;
+import com.voll.med.domain.Pacientes;
 import com.voll.med.dto.MedicoAtualizacaoDto;
 import com.voll.med.dto.MedicoDto;
+import com.voll.med.dto.PacienteDto;
 import com.voll.med.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -20,19 +24,29 @@ public class MedicoController {
     MedicoRepository repository;
 
     @GetMapping("/listar")
-    public Page<MedicoDto> listaMedicos(Pageable pageable){
-        return repository.findAll(pageable).map(MedicoDto::new);
+    public ResponseEntity<Page<MedicoDto>> listaMedicos(Pageable pageable){
+        Page MedicoDto = repository.findAll(pageable).map(MedicoDto::new);
+        return ResponseEntity.ok(MedicoDto);
     }
 
     @PostMapping("/cadastrar")
-    public void cadastrarMedico(@RequestBody MedicoDto medicoDto){
+    public ResponseEntity cadastrarMedico(@RequestBody MedicoDto medicoDto){
         repository.save(new Medicos(medicoDto));
+        return ResponseEntity.ok().build();
     }
 
-    @Transactional
+
     @PutMapping("/atualizar")
-    public void atualizarMedico(@RequestBody @Valid MedicoAtualizacaoDto dto) {
+    @Transactional
+    public ResponseEntity atualizarMedico(@RequestBody @Valid MedicoAtualizacaoDto dto) {
         Medicos medico = repository.getReferenceById(dto.getId());
         medico.atualizarInformacoes(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deletarMedico(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- med/src/main/java/com/voll/med/controller/PacienteController.java
+++ med/src/main/java/com/voll/med/controller/PacienteController.java
@@ -3,12 +3,19 @@ package com.voll.med.controller;
 import com.voll.med.domain.Pacientes;
 import com.voll.med.dto.PacienteDto;
 import com.voll.med.repository.PacienteRepository;
+import org.apache.tomcat.util.digester.ArrayStack;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -18,13 +25,24 @@ public class PacienteController {
     PacienteRepository repository;
 
     @GetMapping("/listar")
-    public Page<PacienteDto> listaPacientes(@PageableDefault(page = 10, sort = {"nome"}) Pageable pageable){
-        return repository.findAll(pageable).map(PacienteDto::new);
+    public ResponseEntity<Page<PacienteDto>> listaPacientes(@PageableDefault(page = 10) Pageable pageable){
+        Page page = repository.findAll(pageable).map(PacienteDto::new);
+        return ResponseEntity.ok(page);
     }
 
-    @Transactional
     @PostMapping("/cadastrar")
-    public void cadastrarPaciente(@RequestBody PacienteDto dto) {
-        repository.save(new Pacientes(dto));
+    @Transactional
+    public ResponseEntity cadastrarPaciente(@RequestBody PacienteDto dto) throws URISyntaxException {
+        Pacientes pacientes = repository.save(new Pacientes(dto));
+        return ResponseEntity.created(new URI("http://localhost:8080/pacientes/")).body(new PacienteDto(pacientes));
+    }
+
+    @PutMapping("/atualizar")
+    @Transactional
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarPaciente(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
